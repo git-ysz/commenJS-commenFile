67d9039a9f1b359648fb0448d2742a3e705b91b7
yao0718 <31951432+git-ysz@users.noreply.github.com>
2018-09-05 11:20:12 +0800

    Update 常用函数.js

--- 常用函数.js
+++ 常用函数.js
@@ -208,3 +208,21 @@ function getDayNum (begin) { //获取起始日到现在的天数
 	var dayNum = parseInt((time-lasttime)/(1000*60*60*24));
 	return dayNum;
 }
+
+// 获取最近N天,count:数字，返回值[date1,data2]
+function timeForMat (count) {
+// 拼接时间
+let time1 = new Date()
+    time1.setTime(time1.getTime() - (24 * 60 * 60 * 1000))
+    let Y1 = time1.getFullYear()
+    let M1 = ((time1.getMonth() + 1) > 10 ? (time1.getMonth() + 1) : '0' + (time1.getMonth() + 1))
+    let D1 = (time1.getDate() > 10 ? time1.getDate() : '0' + time1.getDate())
+    let timer1 = Y1 + '-' + M1 + '-' + D1 // 当前时间
+    let time2 = new Date()
+    time2.setTime(time2.getTime() - (24 * 60 * 60 * 1000 * count))
+    let Y2 = time2.getFullYear()
+    let M2 = ((time2.getMonth() + 1) > 10 ? (time2.getMonth() + 1) : '0' + (time2.getMonth() + 1))
+    let D2 = (time2.getDate() > 10 ? time2.getDate() : '0' + time2.getDate())
+    let timer2 = Y2 + '-' + M2 + '-' + D2 // 之前的7天或者30天
+    return [timer2, timer1];
+}
